9580f47091cb00e50dfa2ec271f6f0da609b3477
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-03-19 18:09:26 +0600

    make price card

--- src/components/PriceOption/PriceOption.jsx
+++ src/components/PriceOption/PriceOption.jsx
@@ -1,8 +1,21 @@
 import PropTypes from 'prop-types';
-const PriceOption = ({ option }) => {
-    return (
-        <div>
+import Feature from '../Feature/Feature';
 
+const PriceOption = ({ option }) => {
+    const { price, name, features } = option;
+    return (
+        <div className='bg-blue-700 rounded-md p-4 text-white'>
+            <h2 className='text-center'>
+                <span className='text-5xl font-bold'>{price}</span>
+
+            </h2>
+            <h2 className='text-2xl text-center my-3'>{name}</h2>
+            <div className='pl-6'>
+                {
+                    features.map((feature, idx) => <Feature key={idx} feature={feature}></Feature>)
+                }
+            </div>
+            <button className='btn mt-6 w-full py-2 font-bold hover:bg-green-700'>Bye Now</button>
         </div>
     );
 };
